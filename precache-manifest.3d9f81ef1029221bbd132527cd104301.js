bb0db4eec693fba39a23f779e610c5b99c1f680e
Alexey Roschektaev <55943475+roschek@users.noreply.github.com>
2021-06-04 09:42:26 +0300

    Add files via upload

---
+++ precache-manifest.3d9f81ef1029221bbd132527cd104301.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e642a2c4dc63c9cb48",
+    "url": "/css/app.9041f3a8.css"
+  },
+  {
+    "revision": "5dac3a50172fe5bff22d853d9de1d689",
+    "url": "/img/login-icon.svg"
+  },
+  {
+    "revision": "f7952d038341368e9fd2d4340f60ed5b",
+    "url": "/img/password-icon.svg"
+  },
+  {
+    "revision": "71236144bf23f4117a5a65df31b4b7c9",
+    "url": "/img/phone-icon.71236144.svg"
+  },
+  {
+    "revision": "71236144bf23f4117a5a65df31b4b7c9",
+    "url": "/img/phone-icon.svg"
+  },
+  {
+    "revision": "837298a055005195e84fd169f8af7522",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66e642a2c4dc63c9cb48",
+    "url": "/js/app.4948e869.js"
+  },
+  {
+    "revision": "ca4f8f424d4d8614f8e1",
+    "url": "/js/chunk-vendors.77db5b69.js"
+  },
+  {
+    "revision": "3a93d560982b9ae17883c19988b6d08d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
